82749672cab95e04048c88481140ebed051caf2c
cyy427 <cyy427@users.noreply.github.com>
2015-11-09 13:21:50 -0500

    Create Example.java
    One example from the dataset.

---
+++ Example.java
@@ -0,0 +1,13 @@
+
+public class Example extends java.util.ArrayList<java.lang.Double> {
+public String toString()
+{
+	String output="";
+   for(int i=0;i<this.size();i++)
+   {
+	   output+=this.get(i)+" ";
+   }
+	   return output;
+}
+
+}
