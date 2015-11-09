eb20a4990b85772f4f980f8cf261334d845d27db
cyy427 <cyy427@users.noreply.github.com>
2015-11-09 13:20:57 -0500

    Create Estimator.java
    Estimator class for Bayes method

---
+++ Estimator.java
@@ -0,0 +1,41 @@
+
+public  class Estimator {
+public int[] count;
+public int sizeofExamples;
+
+void addValue(int k, int l)
+{
+	
+}
+
+Double getProbability(Double double1)
+{
+	return 0.0;
+}
+Double getProbability(int a)
+{
+	return 0.0;
+}
+
+public void outputCount() {
+	// TODO Auto-generated method stub
+	
+}
+
+public void setValue(int k, int indexofclass) {
+	// TODO Auto-generated method stub
+	
+}
+
+public Double getProbability(int i, int j) {
+	return 0.0;
+	// TODO Auto-generated method stub
+	
+}
+
+public double getProbability(Double double1, int classIndex) {
+	// TODO Auto-generated method stub
+	
+	return 0;
+}
+}
