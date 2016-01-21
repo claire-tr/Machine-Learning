1bed1f1dcb692143c3203dfdb391498b6769d4b4
Yunyun Chen <cyy427@users.noreply.github.com>
2016-01-21 13:24:42 -0500

    Update Classifier.java

--- Classifier.java
+++ Classifier.java
@@ -1,21 +1,17 @@
 
 abstract public class Classifier {
-
-
-	
-	int classify(Example e) throws Exception
-	{	return 0;	}
+	int classify(Example e) throws Exception {
+		return 0;
+		}
 	
 	abstract Performance classify(DataSet dataset) throws Exception;
 	
-	Double[] getDistribution(Example e) throws Exception
-	{
+	Double[] getDistribution(Example e) throws Exception {
 		Double[] a= {0.0};
 		return a;
-	}
+		}
 
 	public void train(DataSet train) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+		// TODO Auto-generated method
+		}
 }
