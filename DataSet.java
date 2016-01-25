0183cbab1cf1d0bbf986bc1242b6bcfeb1a1510e
Yunyun Chen <cyy427@users.noreply.github.com>
2016-01-25 14:38:57 -0500

    Update DataSet.java

--- DataSet.java
+++ DataSet.java
@@ -3,7 +3,6 @@ import java.io.FileInputStream;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class DataSet implements OptionHandler{
 	protected Attributes attributes;
 	protected Examples examples;
@@ -30,83 +29,65 @@ public class DataSet implements OptionHandler{
 		// TODO Auto-generated constructor stub
 	}
 
-	void add(DataSet dataset)
-	{
+	void add(DataSet dataset) {
 		this.attributes = dataset.attributes;
 		this.examples = dataset.examples;
 		index = this.attributes.getClassIndex();
 	}
 	
-	void add(Example example)
-	{
+	void add(Example example) {
 		this.examples.add(example);
 	}
-	public Attributes getAttributes()
-	{
+	public Attributes getAttributes() {
 		return this.attributes;
 	}
-	public Examples getExamples()
-	{
+	public Examples getExamples() {
 		return this.examples;
 	}
-	public boolean getHasNominalAttributes()
-	{
-		for(int i=0;i<this.attributes.getSize();i++)
-		{
+	public boolean getHasNominalAttributes() {
+		for (int i=0;i<this.attributes.getSize();i++) {
 			if(attributes.get(i).isNominal())
 				return true;
 		}
 		return false;
 	}
-	public boolean getHasNumericAttributes()
-	{
-		for(int i=0;i<this.attributes.getSize();i++)
-		{
+	public boolean getHasNumericAttributes() {
+		for (int i=0;i<this.attributes.getSize();i++) {
 			if(!attributes.get(i).isNominal())
 				return true;
 		}
 		return false;
 	}
 	
-	public void load(java.lang.String filename) throws java.lang.Exception
-
-		{
+	public void load(java.lang.String filename) throws java.lang.Exception {
 		System.out.println(filename);
 		File file = new File(filename);
 		Scanner scan = new Scanner(new FileInputStream(file));	
-		String datasetname="";		
-		String s="";
+		String datasetname = "";		
+		String s = "";
 		scan.useDelimiter("\n");
-		while(scan.hasNext())
-		{
+		while (scan.hasNext()) {
 				s=scan.next();
-				if(s.contains("@dataset"))
-				{
+				if(s.contains("@dataset")) {
 					
 					String[] q =s.split(" ");
 					datasetname = q[1];
 				}
-				if(s.contains("@attribute")) 
-					{
+				if(s.contains("@attribute")) {
 					String parsing = "";
-					while(s.contains("@attribute"))
-					{
+					while(s.contains("@attribute")) {
 						parsing+=s+"\n";
 						s=scan.next();
 					}
 				//	System.out.println(parsing);
 					Scanner temp = new Scanner(parsing);
 					this.attributes = new Attributes(temp);
-
 					}
-				else if(s.contains("@examples"))
-				{
+				else if(s.contains("@examples")) {
 					String parsing="";
-					while(scan.hasNext())
-					{
+					while(scan.hasNext()) {
 						parsing+=scan.next()+"\n";
 					}
-					
 					Scanner temp = new Scanner(parsing);
 					this.examples = new Examples(this.attributes,temp);
 				}
@@ -117,71 +98,54 @@ public class DataSet implements OptionHandler{
 		index = this.attributes.getClassIndex();
 	          }
 
-	private void parse(java.util.Scanner scanner) throws java.lang.Exception
-            {
-
+	private void parse(java.util.Scanner scanner) throws java.lang.Exception {
             }
-	public void addExamples(Examples examples)
-	{
+	public void addExamples(Examples examples) {
 		this.examples = examples;
 	}
 
-	public java.lang.String toString()
-	{
+	public java.lang.String toString() {
 		String output = "@dataset "+name+"\n\n";
-
 		output+=this.attributes.toString()+"\n";
-		
 		output+=this.examples.toString()+"\n\n\n";
 		return output;
 	}
-	public void setRandom(java.util.Random random)
-	{
+	public void setRandom(java.util.Random random) {
 		this.random = random;
 	}
 	
-	public void setSeed(long seed)
-	{
+	public void setSeed(long seed) {
 		this.seed = seed;
 	}
-	public long getSeed()
-	{
+	public long getSeed() {
 		return this.seed;
 	}
-	public void Partition(int k)
-	{
+	public void Partition(int k) {
 		random = new Random();
 		//do not use seed in this project because the seed is a constant, will generate pseudo-random number
 		//when implementing input a seed from command line, can use seed for generating.
 		partitionflag = new int[this.examples.size()];
-		for(int i=0;i<partitionflag.length;i++)
-		{		
+		for (int i=0;i<partitionflag.length;i++) {		
 			partitionflag[i] = random.nextInt(k);
 			//System.out.println(partitionflag[i]);
 		}
 		isPartitioned = true;
-		
 	}
-	public TrainTestSets getCVSets(int p, int k)
-	{
+	public TrainTestSets getCVSets(int p, int k) {
 		//System.out.println(p+"===");
 		TrainTestSets output = new TrainTestSets();
 		DataSet newTrain = new DataSet(this.attributes);
 		DataSet newTest = new DataSet(this.attributes);
 		//int count1=0, count2=0;
-		if(!isPartitioned)
+		if (!isPartitioned)
 			Partition(k);
 		//build training set
-		for(int i=0;i<this.examples.size();i++)
-		{
-			if(partitionflag[i]==p)
-			{
+		for (int i=0;i<this.examples.size();i++) {
+			if (partitionflag[i]==p) {
 				newTest.add(this.examples.get(i));
 				//count1++;
 			}
-			else
-			{
-
+			else {
 				Example temp = this.examples.get(i);
 				newTrain.add(temp);
 				//count2++;
@@ -190,8 +154,7 @@ public class DataSet implements OptionHandler{
 		//System.out.println(count1+"   "+ count2);
 		output.setTestingSet(newTest);
 		output.setTrainingSet(newTrain);
-		if(output.test.examples.size()<2||output.train.examples.size()<2)
-		{
+		if (output.test.examples.size()<2||output.train.examples.size()<2) {
 			System.out.println("There is not enough examples for each partition,"
 					+ "please choose a new -x value and try again.");
 			System.exit(0);
@@ -202,54 +165,44 @@ public class DataSet implements OptionHandler{
 	public void setOptions(String[] args) {
 		// TODO Auto-generated method stub
 	}
-	int getBestAttribute() throws Exception
-	{
+	int getBestAttribute() throws Exception {
 		index = this.attributes.getClassIndex();
 		Double maxGainRatio=0.0; //the value must >0,if=0,means do not need to split
 		int bestIndex=-1;
-		for(int i=0;i<this.attributes.getSize();i++)
-		{
-			if(i==index) continue;
-			
-				if(getGainRatio(i)>maxGainRatio)
-				{
+		for (int i=0;i<this.attributes.getSize();i++) {
+			if (i==index) continue;
+				if (getGainRatio(i)>maxGainRatio) {
 					maxGainRatio = getGainRatio(i);
 					//System.out.println(maxGainRatio+"DataSet::getBestAttribute");
 					bestIndex = i;
 				}
-			
 		}
-		if(maxGainRatio==0.0) return -1;
+		if (maxGainRatio==0.0) return -1;
 		return bestIndex;
 	}
 
-	DataSet[] partitiononAttribute(int attributeIndex)//return DataSet[]
-	{
+	DataSet[] partitiononAttribute(int attributeIndex)//return DataSet[] {
 		index = this.attributes.getClassIndex();
 		DataSet[] DataSets = new DataSet[this.attributes.get(attributeIndex).domain.size()];
-		for(int i=0;i<DataSets.length;i++)
-		{
+		for(int i=0;i<DataSets.length;i++) {
 			DataSets[i] = new DataSet(this.attributes);
 		}
 		int temp =0;
-		for(int i=0;i<this.examples.size();i++)
-		{
+		for (int i=0;i<this.examples.size();i++) {
 			temp = this.examples.get(i).get(attributeIndex).intValue();
 			DataSets[temp].add(this.examples.get(i));
 		}
 		return DataSets;
 	}
 	
-	Double getGainRatio(int attributeIndex) throws Exception
-	{
-		if(getSplitInformation(attributeIndex)==0)
+	Double getGainRatio(int attributeIndex) throws Exception {
+		if (getSplitInformation(attributeIndex)==0)
 			return 0.0;
 		//System.out.println(attributeIndex);
 		//System.out.println(getGain(attributeIndex)/getSplitInformation(attributeIndex));
 		return getGain(attributeIndex)/getSplitInformation(attributeIndex);
 	}
-	Double getGain(int attributeIndex) throws Exception
-	{
+	Double getGain(int attributeIndex) throws Exception {
 		index = this.attributes.getClassIndex();
 		Double entropy = getEntropy();
 		Double gain = entropy;
@@ -258,27 +211,22 @@ public class DataSet implements OptionHandler{
 		Double [] count = new Double[this.attributes.get(attributeIndex).domain.size()];
 		Double[] s = new Double[count.length];
 		int temp = 0;
-		for(int k=0;k<count.length;k++) count[k] =0.0;
-		for(int i =0;i<this.examples.size();i++)
-		{
+		for (int k=0;k<count.length;k++) count[k] =0.0;
+		for (int i =0;i<this.examples.size();i++) {
 			temp = examples.get(i).get(attributeIndex).intValue();
 			count[temp] += 1;
 		}
-		for(int i=0;i<s.length;i++)
-		{
+		for(int i=0;i<s.length;i++) {
 			s[i] = (double) (count[i]/this.examples.size());
 		}
 		
-		for(int i=0;i<this.attributes.get(attributeIndex).domain.size();i++) //ith attribute
-		{
+		for(int i=0;i<this.attributes.get(attributeIndex).domain.size();i++) //ith attribute {
 			Double all = 0.0;//define Double for the divide computation
 			Double sEntropy = 0.0;
 			Double[] counts = new Double[this.attributes.getClassAttribute().domain.size()];
-			for(int k=0;k<counts.length;k++) counts[k] =0.0;
-			for(int k =0;k<this.examples.size();k++)
-			{
-				if(examples.get(k).get(attributeIndex).intValue()==i)
-				{
+			for (int k=0;k<counts.length;k++) counts[k] =0.0;
+			for (int k =0;k<this.examples.size();k++) {
+				if (examples.get(k).get(attributeIndex).intValue()==i) {
 					temp = examples.get(k).get(index).intValue();
 					counts[temp]+=1;
 					all+=1;
@@ -286,8 +234,7 @@ public class DataSet implements OptionHandler{
 				
 			}
 			
-			for(int j =0;j<this.attributes.getClassAttribute().domain.size();j++)//ith class label
-			{
+			for(int j =0;j<this.attributes.getClassAttribute().domain.size();j++)//ith class label {
 				if(all == 0||counts[j]==0)
 					continue;
 				sEntropy += -(counts[j]/all)*((Math.log((counts[j]/all))/Math.log(2))); 
@@ -302,8 +249,7 @@ public class DataSet implements OptionHandler{
 		return gain;
 	}
 	
-	Double[] getP() throws Exception
-	{
+	Double[] getP() throws Exception {
 		index = this.attributes.getClassIndex();
 		// class p
 		if(pIsCalculated) 
@@ -312,24 +258,20 @@ public class DataSet implements OptionHandler{
 		for(int k=0;k<count.length;k++) count[k] =0.0;
 		p = new Double[count.length];
 		int temp = 0;
-		for(int i =0;i<this.examples.size();i++)
-		{
+		for (int i =0;i<this.examples.size();i++) {
 			temp = examples.get(i).get(index).intValue();
 			count[temp] += 1;
 		}
-		for(int i=0;i<p.length;i++)
-		{
+		for (int i=0;i<p.length;i++) {
 			p[i] = (double) (count[i]/this.examples.size());
 		}
 		pIsCalculated = true;
 		return p;
 	}
-	Double getEntropy() throws Exception
-	{
+	Double getEntropy() throws Exception {
 		p = getP();
 		Double entropy = 0.0;
-		for(int i=0;i<p.length;i++)
-		{
+		for(int i=0;i<p.length;i++) {
 			if(p[i]==0) continue;
 			entropy += -p[i]*(Math.log(p[i])/Math.log(2));
 			//System.out.println(entropy);
@@ -337,21 +279,18 @@ public class DataSet implements OptionHandler{
 //	System.out.println(entropy);
 	return entropy;	
 	}
-	Double getSplitInformation(int attributeIndex)
-	{
+	Double getSplitInformation(int attributeIndex) {
 		index = this.attributes.getClassIndex();
 		Double splitInformation = 0.0;
 		Double [] count = new Double[this.attributes.get(attributeIndex).domain.size()];
 		for(int k=0;k<count.length;k++) count[k] =0.0;
 		Double[] s = new Double[count.length];
 		int temp = 0;
-		for(int i =0;i<this.examples.size();i++)
-		{
+		for(int i =0;i<this.examples.size();i++) {
 			temp = examples.get(i).get(attributeIndex).intValue();
 			count[temp] += 1;
 		}
-		for(int i=0;i<s.length;i++)
-		{
+		for(int i=0;i<s.length;i++) {
 
 			s[i] = (double) (count[i]/this.examples.size());
 			if(s[i]==0.0) continue;
@@ -361,5 +300,4 @@ public class DataSet implements OptionHandler{
 		//System.out.println("splitInformation  "+splitInformation);
 		return splitInformation;
 	}
-
 }
